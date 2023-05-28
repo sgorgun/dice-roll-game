628797b48a7315fca5b33a0ad4b1e31a3b667d78
sgorgun <sergei.gorgun@gmail.com>
2023-05-28 23:34:16 +0300

    refactoring complite

--- Program.cs
+++ Program.cs
@@ -1,66 +1,10 @@
-﻿var random = new Random();
+﻿using DiceRollGame.Game;
+
+var random = new Random();
 var dice = new Dice(random);
 var guessingGame = new GuessingGame(dice);
 
-bool xxx = guessingGame.Play();
+GameResult gameResult = guessingGame.Play();
+GuessingGame.PrintResult(gameResult);
 
 Console.ReadKey();
-
-public class GuessingGame
-{
-    private readonly Dice _dice;
-    private const int InitialTries = 3;
-
-    public GuessingGame(Dice dice)
-    {
-        _dice = dice;
-    }
-
-    public bool Play()
-    {
-        var diceRollResult = _dice.Roll();
-        Console.WriteLine($"Dice rolled. Guess what number it shows in {InitialTries} tries.");
-
-        var tiesLeft = InitialTries;
-        while (tiesLeft > 0)
-        {
-            var guess = ConsoleReader.ReadInteger("Enter a number:");
-            if (guess == diceRollResult)
-            {
-                return true;
-            }
-            Console.WriteLine("Wrong number.");
-            --tiesLeft;
-        }
-        return false;
-    }
-}
-
-public static class ConsoleReader
-{
-    public static int ReadInteger(string message)
-    {
-        int result;
-        do
-        {
-            Console.WriteLine(message);
-        }
-        while (!int.TryParse(Console.ReadLine(), out result));
-        return result;
-    }
-}
-
-public class Dice
-{
-    private readonly Random _random;
-    private const int SidesCount = 6;
-
-    public Dice(Random random)
-    {
-        _random = random;
-    }
-
-    public int Roll() => _random.Next(1, SidesCount + 1);
-
-    public void Describe() => Console.WriteLine($"This is a dice with {SidesCount} sides.");
-}
